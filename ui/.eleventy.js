f17bd8d3a9b0b97bca15bce714b80274df09cefa
Sophie Koonin <sophie@localghost.dev>
2022-09-25 17:02:06 +0100

    Add some basic styling

--- ui/.eleventy.js
+++ ui/.eleventy.js
@@ -1,5 +1,6 @@
 const CleanCSS = require("clean-css")
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget(".")
   eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles
   })
